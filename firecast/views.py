c710a16375e260b4680c6bd69e40a7943bda3924
TheKrakenBeast <TheKrakenBeast@WISEOLDELF>
2018-05-11 02:23:07 +1000

    initial commit for mail service

--- firecast/views.py
+++ firecast/views.py
@@ -1,13 +1,13 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
 # from django.core.mail import EmailMessage
 
 
 class HomePageView(TemplateView):
     template_name = 'firecast/home.html'
 
-# def index(request):
-#    return render(request, 'firecast/index.html', {})
+
 
 
 # email = EmailMessage('Subject', 'Body', to=['projectfirecast666@gmail.com'])
