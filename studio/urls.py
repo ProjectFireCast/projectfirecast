ca2709941e54c8e49d5776c19d5cd9771c2aa251
TheKrakenBeast <TheKrakenBeast@WISEOLDELF>
2018-09-01 23:46:06 +1000

    Restructured site to use blocks. Deleted unused files.

--- studio/urls.py
+++ studio/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from django.conf.urls import url
 
 from . import views
 
 
 urlpatterns = [
     path('record/', views.record, name='record'),
-    path('mix/', views.mix, name='mix'),
 ]
