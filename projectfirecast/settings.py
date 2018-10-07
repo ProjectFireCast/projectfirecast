2d012fab77c69a59a07a3655d1b7d28786693ea1
TheKrakenBeast <37858955+TheKrakenBeast@users.noreply.github.com>
2018-10-07 15:48:14 +1100

    Update settings.py

--- projectfirecast/settings.py
+++ projectfirecast/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3=skqbla)5v!y4)fcf)(mwxe3ab+4plgyi$5fyer8s%t4c68__'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['firecast.com.au']
 
 
 # Application definition
@@ -102,11 +102,7 @@ WSGI_APPLICATION = 'projectfirecast.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    'default': {
+       'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
